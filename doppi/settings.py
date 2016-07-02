a9fb8e7083349983eab39107ba5e5fb012e181ab
utrobin <egor.utrobin@gmail.com>
2016-07-02 20:02:23 +0300

    authentication

--- doppi/settings.py
+++ doppi/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'doppi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'doppi',
+        'USER': 'root',
+        'PASSWORD': '54154167q',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
